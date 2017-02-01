0ec903cd7b40b60040cc813b4a163b1faf309e39
fattire <f4ttire@gmail.com>
2017-02-01 09:42:24 -0800

    lineageos-ify.
    Change-Id: I526aa9d67c177c239f3df99b5aa0da3a37804b65

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,11 +16,8 @@
 
 $(call inherit-product, device/samsung/trltetmo/full_trltetmo.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit common CM phone.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := trltetmo
-PRODUCT_NAME := cm_trltetmo
+PRODUCT_NAME := lineage_trltetmo
