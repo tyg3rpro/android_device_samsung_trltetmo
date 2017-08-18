8da86f5fb2ff531c70bf91239cbc04f98dccb556
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    trltetmo: init: Update for O
    Change-Id: I60475e3dbf68329a46c421e8c8465a4ea2e28203

--- init/init_trlte.cpp
+++ init/init_trlte.cpp
@@ -31,12 +31,15 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "vendor_init.h"
+#include <android-base/properties.h>
+
 #include "property_service.h"
+#include "vendor_init.h"
 #include "log.h"
-#include "util.h"
 
 #include "init_apq8084.h"
+
+using android::base::GetProperty;
 
 void gsm_properties()
 {
@@ -46,11 +49,11 @@ void gsm_properties()
 
 void init_target_properties()
 {
-    std::string platform = property_get("ro.board.platform");
+    std::string platform = GetProperty("ro.board.platform", "");
     if (platform != ANDROID_TARGET)
         return;
 
-    std::string bootloader = property_get("ro.bootloader");
+    std::string bootloader = GetProperty("ro.bootloader", "");
 
     if (bootloader.find("N910T") == 0) {
         property_override("ro.build.fingerprint", "samsung/trltetmo/trltetmo:6.0.1/MMB29M/N910TUVS2EQG1:user/release-keys");
@@ -67,9 +70,9 @@ void init_target_properties()
         property_override("ro.product.name", "trltecan");
         gsm_properties();
     } else {
-        ERROR("Setting product info FAILED\n");
+        gsm_properties();
     }
 
-    std::string device = property_get("ro.product.device");
-    INFO("Found bootloader id %s setting build properties for %s device\n", bootloader.c_str(), device.c_str());
+    std::string device = GetProperty("ro.product.device", "");
+    LOG(INFO) << "Found bootloader id " << bootloader << " setting build properties for " << device << " device" << std::endl;
 }
