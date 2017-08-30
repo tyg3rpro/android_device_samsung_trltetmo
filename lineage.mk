7feca9562874ddaeba8be73ea26233851e615a34
Kevin F. Haggerty <haggertk@lineageos.org>
2017-08-30 06:45:00 -0600

    trltetmo: Inherit from vendor/lineage
    Change-Id: Iadc568801de6b3b135310441d20323b9144b7e0a

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,8 +16,8 @@
 
 $(call inherit-product, device/samsung/trltetmo/full_trltetmo.mk)
 
-# Inherit common CM phone.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit common LineageOS phone.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := trltetmo
 PRODUCT_NAME := lineage_trltetmo
