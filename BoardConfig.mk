3e9a8a25fe84047e59d0e436de489f6fbdaaf718
underscoremone <underscoremone@gmail.com>
2017-11-26 15:15:28 +0000

    trltetmo: BoardConfig: Declare partitions sizes
    Change-Id: Ibca81ac6b48010725b22a1d8c8dfc07617c99b40

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,5 +24,12 @@ TARGET_KERNEL_VARIANT_CONFIG := apq8084_sec_trlte_tmo_defconfig
 TARGET_INIT_VENDOR_LIB := libinit_apq8084
 TARGET_LIBINIT_APQ8084_DEFINES_FILE := device/samsung/trltetmo/init/init_trlte.cpp
 
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 262144
+BOARD_BOOTIMAGE_PARTITION_SIZE := 17825792
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 19932160
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3774873600
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 27040657408
+
 # inherit from the proprietary version
 -include vendor/samsung/trltetmo/BoardConfigVendor.mk
